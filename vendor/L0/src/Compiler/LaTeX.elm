1577909c16feee25947db8b23631653bf75de94f
James Carlson <jxxcarlson@gmail.com>
2022-02-25 04:50:06 -0500

    Update Compiler.LaTeX

--- vendor/L0/src/Compiler/LaTeX.elm
+++ vendor/L0/src/Compiler/LaTeX.elm
@@ -9,11 +9,11 @@ b =
     ExpressionBlock { args = [], blockType = Paragraph, children = [], content = Right [ Expr "title" [ Text "foo" { begin = 7, end = 9, index = 3 } ] { begin = 0, end = 0, index = 0 } ], id = "0", indent = 1, lineNumber = 0, messages = [], name = Nothing, numberOfLines = 1, sourceText = "\\title{foo}" }
 
 
-ordinaryBlock args str data m1 =
+ordinaryBlock args exprs data m1 =
     ExpressionBlock
         { data
             | blockType = OrdinaryBlock args
-            , content = Right [ Text str m1 ]
+            , content = Right exprs
             , args = List.drop 1 args
             , name = List.head args
         }
@@ -24,20 +24,20 @@ transform ((ExpressionBlock data) as block) =
     case data.content of
         --Right [ Expr "title" [ Text str m1 ] m2 ] ->
         --    ordinaryBlock "title" str data m1
-        Right [ _, Expr "section" [ Text str m1 ] m2 ] ->
-            ordinaryBlock [ "heading", "1" ] str data m1
+        Right [ _, Expr "section" exprs m2 ] ->
+            ordinaryBlock [ "heading", "1" ] exprs data m2
 
-        Right [ _, Expr "subsection" [ Text str m1 ] m2 ] ->
-            ordinaryBlock [ "heading", "2" ] str data m1
+        Right [ _, Expr "subsection" exprs m2 ] ->
+            ordinaryBlock [ "heading", "2" ] exprs data m2
 
-        Right [ _, Expr "subsubsection" [ Text str m1 ] m2 ] ->
-            ordinaryBlock [ "heading", "3" ] str data m1
+        Right [ _, Expr "subsubsection" exprs m2 ] ->
+            ordinaryBlock [ "heading", "3" ] exprs data m2
 
-        Right [ _, Expr "subheading" [ Text str m1 ] m2 ] ->
-            ordinaryBlock [ "heading", "4" ] str data m1
+        Right [ _, Expr "subheading" exprs m2 ] ->
+            ordinaryBlock [ "heading", "4" ] exprs data m2
 
         Right [ _, Expr "makeTableOfContents" [] m2 ] ->
-            ordinaryBlock [ "makeTableOfContents" ] "" data m2
+            ordinaryBlock [ "makeTableOfContents" ] [] data m2
 
         _ ->
             block
