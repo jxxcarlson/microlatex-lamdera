862588b3f0a7c398e52078898996eaf491d8f25e
James Carlson <jxxcarlson@gmail.com>
2022-03-04 07:51:56 -0500

    Display "New" button on startup

--- src/View/Main.elm
+++ src/View/Main.elm
@@ -287,11 +287,11 @@ messageRow model =
 header model width_ =
     E.row [ E.spacing 12, E.width E.fill ]
         [ View.Utility.hideIf (model.currentUser == Nothing) (Button.cycleLanguage model.language)
-        , View.Utility.hideIf (model.currentUser == Nothing) Button.newDocument
-        , View.Utility.hideIf (model.currentUser == Nothing) (Button.deleteDocument model)
-        , View.Utility.hideIf (model.currentUser == Nothing) (Button.cancelDeleteDocument model)
         , View.Utility.showIf model.showEditor Button.closeEditor
         , View.Utility.hideIf model.showEditor Button.openEditor
+        , Button.newDocument
+        , View.Utility.hideIf (model.currentUser == Nothing) (Button.deleteDocument model)
+        , View.Utility.hideIf (model.currentUser == Nothing) (Button.cancelDeleteDocument model)
         , View.Utility.hideIf (model.currentUser == Nothing) (View.Utility.showIf model.showEditor (Button.togglePublic model.currentDocument))
         , View.Utility.showIf model.showEditor (wordCount model)
         , E.el [ Font.size 14, Font.color (E.rgb 0.9 0.9 0.9) ] (E.text (currentAuthor model.currentDocument))
