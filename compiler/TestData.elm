30c086fb8f9575500496bfec311da40a18ff8abe
James Carlson <jxxcarlson@gmail.com>
2022-03-06 09:15:59 -0500

    Add: TestData.elm

---
+++ compiler/TestData.elm
@@ -0,0 +1,42 @@
+module TestData exposing (..)
+
+
+m1 =
+    """
+\\begin{theorem}
+  this is a test.
+  
+  $$
+  c^2
+  $$
+  
+\\end{theorem}
+"""
+
+
+l1 =
+    """| theorem
+  this is a test.
+  
+  $$
+  c^2
+  $$
+
+"""
+
+
+l2 =
+    """| theorem
+  TEST 1
+  
+  $$
+  x^2
+  $$
+
+| theorem
+  TEST 2
+  
+  $$
+  y^2
+  $$
+"""
