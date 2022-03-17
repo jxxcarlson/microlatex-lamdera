1cbb2a1e6ecc5b17d8b06854648a75c5a4164798
James Carlson <jxxcarlson@gmail.com>
2022-03-17 18:27:00 -0400

    Update tests

--- tests/PrimitiveBlock.elm
+++ tests/PrimitiveBlock.elm
@@ -16,10 +16,21 @@ test_ label expr expectedOutput =
     test label <| \_ -> equal expr expectedOutput
 
 
+
+{-
+   FOR THE MOMENT: sequences of two or more newlines parse to [""].
+   That way there is an effient representation of the end-of-block marker.
+   Not sure if this is a good idea.
+
+
+-}
+
+
 suite : Test
 suite =
     describe "The primitive block parser"
-        [ test_ "two paragraphs" (bllc "abc\ndef\n\nghi\njkl") [ [ "abc", "def" ], [ "ghi", "jkl" ] ]
-        , test_ "two paragraphs (2)" (bllc "abc\n  def\n\nghi\njkl") [ [ "abc", "  def" ], [ "ghi", "jkl" ] ]
-        , test_ "two paragraphs (3)" (bllc "abc\ndef\n\n\nghi\njkl") [ [ "abc", "def" ], [ "ghi", "jkl" ] ]
+        [ test_ "two paragraphs" (bllc "abc\ndef\n\nghi\njkl") [ [ "abc", "def" ], [ "" ], [ "ghi", "jkl" ] ]
+        , test_ "two paragraphs (2)" (bllc "abc\n  def\n\nghi\njkl") [ [ "abc", "  def" ], [ "" ], [ "ghi", "jkl" ] ]
+        , test_ "two paragraphs (3)" (bllc "abc\ndef\n\n\nghi\njkl") [ [ "abc", "def" ], [ "" ], [ "ghi", "jkl" ] ]
+        , test_ "two paragraphs (4)" (bllc "abc\ndef\n\n\n\nghi\njkl") [ [ "abc", "def" ], [ "" ], [ "ghi", "jkl" ] ]
         ]
