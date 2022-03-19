ce9287aed08e75a2ed2da505774c4f2122ce6292
James Carlson <jxxcarlson@gmail.com>
2022-03-18 22:09:03 -0400

    Disable Debug.log in Tools

--- compiler/Tools.elm
+++ compiler/Tools.elm
@@ -20,12 +20,14 @@ cyan label width =
 
 debugLog1 : String -> a -> a
 debugLog1 label a =
-    Debug.log (label |> Console.bgBlue |> Console.white) a
+    --Debug.log (label |> Console.bgBlue |> Console.white) a
+    a
 
 
 debugLog2 : String -> (a -> b) -> a -> a
 debugLog2 label f a =
-    Debug.log (label ++ ":: " ++ Debug.toString (f a) |> Console.bgCyan |> Console.black) a
+    --Debug.log (label ++ ":: " ++ Debug.toString (f a) |> Console.bgCyan |> Console.black) a
+    a
 
 
 
