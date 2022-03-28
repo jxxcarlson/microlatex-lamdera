23f966479aeeeb0f81c0f0ca338ad5a0fe263c8c
James Carlson <jxxcarlson@gmail.com>
2022-03-27 21:05:45 -0400

    Add function isVerbatimLine

--- compiler/L0/Parser/Classify.elm
+++ compiler/L0/Parser/Classify.elm
@@ -1,9 +1,16 @@
-module L0.Parser.Classify exposing (classify)
+module L0.Parser.Classify exposing (classify, isVerbatimLine)
 
 import Parser.Block exposing (BlockType(..))
 import Parser.Common exposing (Classification)
 import Parser.Line exposing (PrimitiveBlockType(..))
 import Parser.PrimitiveBlock exposing (PrimitiveBlock)
+
+
+isVerbatimLine : String -> Bool
+isVerbatimLine str =
+    (String.left 2 str == "||")
+        || (String.left 3 str == "```")
+        || (String.left 2 str == "$$")
 
 
 classify : PrimitiveBlock -> Classification
