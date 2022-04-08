5d153f449909c00a003ba622158b66af46b0b65e
James Carlson <jxxcarlson@gmail.com>
2022-04-08 18:06:30 -0400

    Better formatting of date - time

--- src/DateTimeUtility.elm
+++ src/DateTimeUtility.elm
@@ -1,30 +1,23 @@
-module DateTimeUtility exposing (toUtcSlug, toUtcString)
+module DateTimeUtility exposing (toString)
 
 import Time exposing (Month(..), toDay, toHour, toMonth, toYear, utc)
 
 
-toUtcString : Time.Posix -> String
-toUtcString time =
-    monthString (Time.toMonth utc time)
+toString : Time.Zone -> Time.Posix -> String
+toString zone time =
+    monthString (Time.toMonth zone time)
         ++ "/"
-        ++ String.fromInt (Time.toDay utc time)
+        ++ (String.fromInt (Time.toDay zone time) |> String.padLeft 2 '0')
+        ++ "/"
+        ++ String.fromInt (Time.toYear zone time)
+        ++ " – "
+        ++ (String.fromInt (toHour zone time) |> String.padLeft 2 '0')
         ++ ":"
-        ++ String.fromInt (toHour utc time)
-        ++ ":"
-        ++ String.fromInt (Time.toMinute utc time)
+        ++ (String.fromInt (Time.toMinute zone time) |> String.padLeft 2 '0')
 
 
-toUtcSlug : String -> String -> Time.Posix -> String
-toUtcSlug str1 str2 time =
-    (toMonth utc time |> monthString)
-        ++ "-"
-        ++ String.fromInt (toDay utc time)
-        ++ "-"
-        ++ String.fromInt (toYear utc time)
-        ++ "-"
-        ++ str1
-        ++ (toHour utc time |> String.fromInt)
-        ++ str2
+
+--
 
 
 monthString : Time.Month -> String
