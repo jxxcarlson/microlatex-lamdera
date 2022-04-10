0a4f4f8215e691d0e8a2c1b0d4e0ca03c35a7a2c
James Carlson <jxxcarlson@gmail.com>
2022-04-10 07:01:11 -0400

    Display count of tags and items per tag in sidebar

--- src/View/Sidebar.elm
+++ src/View/Sidebar.elm
@@ -29,14 +29,25 @@ view model =
                 ]
                 [ E.row [ E.spacing 12, E.paddingEach { top = 12, bottom = 0, left = 0, right = 0 } ] [ Button.getUserTags model.tagSelection model.currentUser, Button.getPublicTags model.tagSelection ]
                 , search model
-                , E.column
-                    [ E.scrollbarY
-                    , E.width (E.px Geometry.sidebarWidth)
-                    , E.spacing 4
-                    , E.height (E.px (Geometry.appHeight_ model - 190))
-                    ]
-                    (viewTagDict model.inputSearchTagsKey model.tagDict)
+                , viewTagDict model
                 ]
+
+
+viewTagDict model =
+    let
+        dictItems =
+            Dict.toList model.tagDict
+
+        header =
+            E.el [ Font.size 14, E.paddingXY 2 4 ] (E.text <| "Tags: " ++ (List.length dictItems |> String.fromInt))
+    in
+    E.column
+        [ E.scrollbarY
+        , E.width (E.px Geometry.sidebarWidth)
+        , E.spacing 4
+        , E.height (E.px (Geometry.appHeight_ model - 190))
+        ]
+        (header :: viewTagDict_ model.inputSearchTagsKey dictItems)
 
 
 search model =
@@ -60,10 +71,9 @@ searchTags key_ list =
         List.filter (\item -> String.contains key item.tag || String.contains key (String.toLower item.title)) list
 
 
-viewTagDict : String -> Dict String (List { a | id : String, title : String }) -> List (Element FrontendMsg)
-viewTagDict key dict =
-    dict
-        |> Dict.toList
+viewTagDict_ : String -> List ( String, List { a | id : String, title : String } ) -> List (Element FrontendMsg)
+viewTagDict_ key dictItems =
+    dictItems
         |> List.map (\( tag, list ) -> List.map (\item -> { tag = tag, id = item.id, title = item.title }) list)
         |> List.map (searchTags key)
         |> List.map viewTagGroup
@@ -71,12 +81,17 @@ viewTagDict key dict =
 
 viewTagGroup : List { tag : String, id : String, title : String } -> Element FrontendMsg
 viewTagGroup list =
+    let
+        n =
+            " (" ++ (List.length list |> String.fromInt) ++ ")"
+    in
     case List.head list of
         Nothing ->
             E.none
 
         Just headItem ->
-            E.column [ E.spacing 2, E.paddingEach { top = 8, bottom = 0, left = 0, right = 0 } ] (E.el [ E.paddingXY 6 0, Font.size 14 ] (E.text headItem.tag) :: List.map viewTagDictItem (List.sortBy .title list))
+            E.column [ E.spacing 2, E.paddingEach { top = 8, bottom = 0, left = 0, right = 0 } ]
+                (E.el [ E.paddingXY 6 0, Font.size 14 ] (E.text (headItem.tag ++ n)) :: List.map viewTagDictItem (List.sortBy .title list))
 
 
 viewTagDictItem : { tag : String, id : String, title : String } -> Element FrontendMsg
