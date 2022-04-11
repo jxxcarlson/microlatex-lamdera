8efc3749939b6bb0bd2e6118ee06ebe19ef9ffeb
James Carlson <jxxcarlson@gmail.com>
2022-04-11 13:59:44 -0400

    remove debug

--- src/Share.elm
+++ src/Share.elm
@@ -164,14 +164,12 @@ narrowCast sendersName document connectionDict =
         Document.ShareWith { editors, readers } ->
             let
                 usernames =
-                    (case document.author of
+                    case document.author of
                         Nothing ->
                             editors ++ readers |> List.filter (\name -> name /= sendersName && name /= "")
 
                         Just author ->
                             author :: (editors ++ readers) |> List.filter (\name -> name /= sendersName && name /= "")
-                    )
-                        |> Debug.log "sh.nc, USERNAMES (1)"
 
                 clientIds =
                     getClientIds usernames connectionDict
