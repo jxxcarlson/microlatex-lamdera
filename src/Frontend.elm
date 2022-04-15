f8f5bac5bf3c4596f809d5b772cffb738be66c6d
James Carlson <jxxcarlson@gmail.com>
2022-04-15 15:18:53 -0400

    missing paren

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -970,7 +970,7 @@ updateFromBackend msg model =
             in
             case List.head documents of
                 Nothing ->
-                    ( model, sendToBackend (FetchDocumentById StandardHandling Config.notFoundDocId)
+                    ( model, sendToBackend (FetchDocumentById StandardHandling Config.notFoundDocId) )
 
                 Just doc ->
                     ( { model | documents = documents, currentDocument = Just doc }, Util.delay 40 (SetDocumentCurrent doc) )
