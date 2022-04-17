b4dee3b693345293fa9f3c1e0c2b9ebf23317e81
James Carlson <jxxcarlson@gmail.com>
2022-04-16 20:38:20 -0400

    Add Martin's new network code (types)

--- src/Network.elm
+++ src/Network.elm
@@ -1,50 +1,18 @@
-module Network exposing (NetworkModel, init, localState, updateFromBackend, updateFromUser)
+module Network exposing (..)
 
-import AbstractNetwork
-import Diff
-import Diff.Change
-import List.Extra
+import Dict exposing (Dict)
 
 
-type alias NetworkModel =
-    AbstractNetwork.NetworkModel NetworkMessage Data
-
-generateDiffMessage : String -> String -> NetworkMessage
-generateDiffMessage oldString newString =
+type alias UserId =
+    String
 
 
-type alias Data =
-    List String
+type Event
+    = MovedCursor UserId { xOffset : Int, yOffset : Int } -- Offset relative to the previous cursor position
+    | TypedText UserId String
 
 
-type alias NetworkMessage =
-    List (List (Diff.Change String))
-
-
-init : List String -> NetworkModel
-init =
-    AbstractNetwork.init
-
-
-updateFromUser : NetworkMessage -> NetworkModel -> NetworkModel
-updateFromUser msg localModel =
-    { localMsgs = localModel.localMsgs ++ [ msg ]
-    , serverState = localModel.serverState
-    }
-
-
-updateFunc : NetworkMessage -> Data -> Data
-updateFunc =
-    Diff.Change.reconcileList
-
-
-localState : NetworkModel -> Data
-localState localModel =
-    List.foldl updateFunc localModel.serverState localModel.localMsgs
-
-
-updateFromBackend : NetworkMessage -> NetworkModel -> NetworkModel
-updateFromBackend msg localModel =
-    { localMsgs = List.Extra.remove msg localModel.localMsgs
-    , serverState = updateFunc msg localModel.serverState
+type alias ServerState =
+    { cursorPositions : Dict UserId { x : Int, y : Int }
+    , document : String
     }
---
+++ src/NetworkOld.elm
@@ -0,0 +1,52 @@
+module NetworkOld exposing (NetworkModel, init, localState, updateFromBackend, updateFromUser)
+
+import AbstractNetwork
+import Diff
+import Diff.Change
+import List.Extra
+
+
+type alias NetworkModel =
+    AbstractNetwork.NetworkModel NetworkMessage Data
+
+
+
+generateDiffMessage : String -> String -> NetworkMessage
+generateDiffMessage oldString newString =
+
+
+type alias Data =
+    List String
+
+
+type alias NetworkMessage =
+    List (List (Diff.Change String))
+
+
+init : List String -> NetworkModel
+init =
+    AbstractNetwork.init
+
+
+updateFromUser : NetworkMessage -> NetworkModel -> NetworkModel
+updateFromUser msg localModel =
+    { localMsgs = localModel.localMsgs ++ [ msg ]
+    , serverState = localModel.serverState
+    }
+
+
+updateFunc : NetworkMessage -> Data -> Data
+updateFunc =
+    Diff.Change.reconcileList
+
+
+localState : NetworkModel -> Data
+localState localModel =
+    List.foldl updateFunc localModel.serverState localModel.localMsgs
+
+
+updateFromBackend : NetworkMessage -> NetworkModel -> NetworkModel
+updateFromBackend msg localModel =
+    { localMsgs = List.Extra.remove msg localModel.localMsgs
+    , serverState = updateFunc msg localModel.serverState
+    }
