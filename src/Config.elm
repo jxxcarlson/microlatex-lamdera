d49c165484fa66b1ebf9e83407f3f8991f0831db
James Carlson <jxxcarlson@gmail.com>
2022-04-21 00:41:24 -0400

    Experimental auto signout period in production for testing

--- src/Config.elm
+++ src/Config.elm
@@ -74,8 +74,8 @@ backendTickSeconds =
 automaticSignoutLimit =
     case Env.mode of
         Env.Production ->
-            -- one hour
-            3600
+            -- two minutes
+            120
 
         Env.Development ->
             20
@@ -86,8 +86,8 @@ automaticSignoutLimit =
 automaticSignoutNoticePeriod =
     case Env.mode of
         Env.Production ->
-            -- 10 minutes
-            600
+            -- 1 minute
+            60
 
         Env.Development ->
             5
