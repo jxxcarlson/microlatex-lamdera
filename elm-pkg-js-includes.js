d0350a443d2179a85f8e32287ebef576801f9b8b
James Carlson <jxxcarlson@gmail.com>
2022-04-24 15:23:13 -0400

    exchange order of katex and mhchem (katex last)

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -10,9 +10,9 @@ exports.init = async function init(app) {
   // @WARNING: this only runs for Lamdera production deploys!
   // This file will not run in Local development, an equivalent to this is
   // automatically generated in Local Development for every file in elm-pkg-js/
-  elm_katex.init(app)
-  codemirror_element.init(app)
   mhchem_package.init(app)
+  codemirror_element.init(app)
+  elm_katex.init(app)
 
 
 }
