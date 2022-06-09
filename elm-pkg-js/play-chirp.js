fc32e5d9d253edce064bb85d68b91dd3e212058d
James Carlson <jxxcarlson@gmail.com>
2022-06-10 00:46:43 +0200

    Add consoloe.log message to playscript

--- elm-pkg-js/play-chirp.js
+++ elm-pkg-js/play-chirp.js
@@ -4,6 +4,7 @@ port play_chirp : Cmd msg
 
 exports.init = async function init(app) {
   app.ports.playChirp.subscribe( function() {
+   console.log("Starting play-chirp");
    var audio = new Audio('chirp.mp3');
    audio.play();
   })
