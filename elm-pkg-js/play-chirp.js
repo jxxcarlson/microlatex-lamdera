2019fde65961c854dfde6796afc493213df73ebc
James Carlson <jxxcarlson@gmail.com>
2022-06-10 01:47:55 +0200

    Add boing-short sound and use it instead of the chirp mp3

--- elm-pkg-js/play-chirp.js
+++ elm-pkg-js/play-chirp.js
@@ -5,7 +5,7 @@ port playChirp : Cmd msg
 exports.init = async function init(app) {
   app.ports.playChirp.subscribe( function() {
    console.log("Starting play-chirp");
-   var audio = new Audio('chirp.mp3');
+   var audio = new Audio('boing-short.mp3');
    audio.play();
   })
 }
