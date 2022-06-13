de8623659ddc44b2a12607488fb457f0ba1f3cc5
James Carlson <jxxcarlson@gmail.com>
2022-06-13 16:12:28 +0200

    Use postprocessDocument in setDocumentAsCurrent_, branch in which currentUser == Nothing.  This is to render an incoming document where the app is activated by a link

--- src/Frontend/Update.elm
+++ src/Frontend/Update.elm
@@ -640,7 +640,14 @@ setDocumentAsCurrent_ : Cmd FrontendMsg -> FrontendModel -> Document.Document ->
 setDocumentAsCurrent_ cmd model doc permissions =
     case model.currentUser of
         Nothing ->
-            ( model, Cmd.none )
+            let
+                _ =
+                    Debug.log "CURRENT DOC WILL BE " doc.title
+
+                newModel =
+                    postProcessDocument doc model
+            in
+            ( { newModel | currentDocument = Just doc }, Cmd.none )
 
         Just currentUser ->
             let
