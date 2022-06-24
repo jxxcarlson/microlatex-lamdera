61efe8c490f0010f5ab63a3dee6be25adf99ac8e
James Carlson <jxxcarlson@gmail.com>
2022-06-24 07:18:26 -0400

    Remove unneeded code

--- src/CollaborativeEditing/NetworkModel.elm
+++ src/CollaborativeEditing/NetworkModel.elm
@@ -3,16 +3,12 @@ module CollaborativeEditing.NetworkModel exposing
     , NetworkModel
     , ServerState
     , applyEvent
-    , applyEvent2
     , applyLocalEvents
     , createEvent
-    , emptyServerState
     , getLocalDocument
     , init
-    , initWithUserAndContent
     , initWithUsersAndContent
     , initialServerState
-    , manyUserInitialServerState
     , nullEvent
     , shortenDictKeys
     , toString1
@@ -37,22 +33,6 @@ type alias ServerState =
     }
 
 
-applyLocalEvents : (EditEvent -> ServerState -> ServerState) -> NetworkModel -> NetworkModel
-applyLocalEvents updateFunc localModel =
-    let
-        newServerState =
-            List.foldl (\event serverState -> updateFunc event serverState) localModel.serverState localModel.localMsgs
-    in
-    { localMsgs = [], serverState = newServerState }
-
-
-updateFromBackend2 : (EditEvent -> ServerState -> ServerState) -> EditEvent -> NetworkModel -> NetworkModel
-updateFromBackend2 updateFunc event localModel =
-    { localMsgs = List.Extra.remove event localModel.localMsgs
-    , serverState = updateFunc event localModel.serverState
-    }
-
-
 type alias UserId =
     String
 
@@ -63,6 +43,134 @@ type alias DocId =
 
 type alias EditEvent =
     { docId : String, userId : String, dp : Int, operations : List OT.Operation }
+
+
+
+-- INIT
+
+
+init : ServerState -> NetworkModel
+init serverState =
+    { localMsgs = [], serverState = serverState }
+
+
+initWithUsersAndContent : DocId -> List UserId -> String -> NetworkModel
+initWithUsersAndContent docId userIds content =
+    init (manyUserInitialServerState docId userIds content)
+
+
+initialServerState : DocId -> UserId -> String -> ServerState
+initialServerState docId userId content =
+    { cursorPositions = Dict.fromList [ ( userId, 0 ) ]
+    , document = { docId = docId, cursor = 0, content = content }
+    }
+
+
+manyUserInitialServerState : DocId -> List UserId -> String -> ServerState
+manyUserInitialServerState docId userIds content =
+    { cursorPositions = Dict.fromList (List.map (\id -> ( id, 0 )) userIds)
+    , document = { docId = docId, cursor = 0, content = content }
+    }
+
+
+
+-- UPDATE
+
+
+updateFromUser : EditEvent -> NetworkModel -> NetworkModel
+updateFromUser event localModel =
+    { localMsgs = localModel.localMsgs ++ [ event ]
+    , serverState = localModel.serverState
+    }
+
+
+updateFromBackend : (EditEvent -> ServerState -> ServerState) -> EditEvent -> NetworkModel -> NetworkModel
+updateFromBackend updateFunc event localModel =
+    { localMsgs = List.Extra.remove event localModel.localMsgs
+    , serverState = updateFunc event localModel.serverState
+    }
+
+
+
+-- LOCAL
+
+
+localState : (EditEvent -> ServerState -> ServerState) -> NetworkModel -> ServerState
+localState updateFunc localModel =
+    List.foldl updateFunc localModel.serverState localModel.localMsgs
+
+
+getLocalDocument : NetworkModel -> OT.Document
+getLocalDocument localModel =
+    localState applyEvent localModel |> .document
+
+
+
+-- ENCODE
+
+
+encodeEvent : { counter : Int, cursor : Int, event : EditEvent } -> E.Value
+encodeEvent { counter, cursor, event } =
+    E.object
+        [ ( "name", E.string "OTOp" )
+        , ( "dp", E.int event.dp )
+        , ( "ops", E.list OT.encodeOperation event.operations )
+        , ( "cursor", E.int cursor )
+        , ( "counter", E.int counter )
+        ]
+
+
+nullEvent : E.Value
+nullEvent =
+    E.object [ ( "name", E.string "OTNull" ) ]
+
+
+
+-- CREATE AND APPLY EVENTS
+
+
+createEvent : String -> OT.Document -> OT.Document -> EditEvent
+createEvent userId_ oldDocument newDocument =
+    let
+        dp =
+            newDocument.cursor - oldDocument.cursor
+
+        operations : List OT.Operation
+        operations =
+            OT.findOps oldDocument newDocument
+    in
+    { docId = oldDocument.docId, userId = userId_, dp = dp, operations = operations }
+
+
+applyLocalEvents : (EditEvent -> ServerState -> ServerState) -> NetworkModel -> NetworkModel
+applyLocalEvents updateFunc localModel =
+    let
+        newServerState =
+            List.foldl (\event serverState -> updateFunc event serverState) localModel.serverState localModel.localMsgs
+    in
+    { localMsgs = [], serverState = newServerState }
+
+
+applyEvent : EditEvent -> ServerState -> ServerState
+applyEvent event serverState =
+    case Dict.get event.userId serverState.cursorPositions of
+        Nothing ->
+            --serverState
+            -- TODO: changed from 'serverState' to the below temporarily
+            -- TODO: so that events from different user will be applied.
+            -- TODO: Need to understand the process: why the Dict.get ...
+            { cursorPositions = serverState.cursorPositions
+            , document = OT.apply event.operations serverState.document
+            }
+
+        Just p ->
+            { cursorPositions = Dict.insert event.userId (p + event.dp) serverState.cursorPositions
+            , document = OT.apply event.operations serverState.document
+            }
+
+
+
+-- TO STRING
 
 
 toStringList : EditEvent -> { ids : String, dp : String, ops : String }
@@ -96,118 +204,3 @@ toString1 { counter, cursor, event } =
 
         Just event_ ->
             { counter = counter, cursor = cursor, event = event_ } |> encodeEvent |> E.encode 2
-
-
-encodeEvent : { counter : Int, cursor : Int, event : EditEvent } -> E.Value
-encodeEvent { counter, cursor, event } =
-    E.object
-        [ ( "name", E.string "OTOp" )
-        , ( "dp", E.int event.dp )
-        , ( "ops", E.list OT.encodeOperation event.operations )
-        , ( "cursor", E.int cursor )
-        , ( "counter", E.int counter )
-        ]
-
-
-nullEvent : E.Value
-nullEvent =
-    E.object [ ( "name", E.string "OTNull" ) ]
-
-
-initialServerState : DocId -> UserId -> String -> ServerState
-initialServerState docId userId content =
-    { cursorPositions = Dict.fromList [ ( userId, 0 ) ]
-    , document = { docId = docId, cursor = 0, content = content }
-    }
-
-
-manyUserInitialServerState : DocId -> List UserId -> String -> ServerState
-manyUserInitialServerState docId userIds content =
-    { cursorPositions = Dict.fromList (List.map (\id -> ( id, 0 )) userIds)
-    , document = { docId = docId, cursor = 0, content = content }
-    }
-
-
-emptyServerState =
-    { cursorPositions = Dict.empty
-    , document = OT.emptyDoc
-    }
-
-
-createEvent : String -> OT.Document -> OT.Document -> EditEvent
-createEvent userId_ oldDocument newDocument =
-    let
-        dp =
-            newDocument.cursor - oldDocument.cursor
-
-        operations : List OT.Operation
-        operations =
-            OT.findOps oldDocument newDocument
-    in
-    { docId = oldDocument.docId, userId = userId_, dp = dp, operations = operations }
-
-
-applyEvent : EditEvent -> ServerState -> ServerState
-applyEvent event serverState =
-    case Dict.get event.userId serverState.cursorPositions of
-        Nothing ->
-            --serverState
-            { cursorPositions = serverState.cursorPositions
-            , document = OT.apply event.operations serverState.document
-            }
-
-        Just p ->
-            { cursorPositions = Dict.insert event.userId (p + event.dp) serverState.cursorPositions
-            , document = OT.apply event.operations serverState.document
-            }
-
-
-applyEvent2 : EditEvent -> ServerState -> ServerState
-applyEvent2 event serverState =
-    case Dict.get event.userId serverState.cursorPositions of
-        Nothing ->
-            serverState
-
-        Just p ->
-            { cursorPositions = Dict.insert event.userId (p + event.dp) serverState.cursorPositions
-            , document = OT.apply event.operations serverState.document
-            }
-
-
-init : ServerState -> NetworkModel
-init serverState =
-    { localMsgs = [], serverState = serverState }
-
-
-initWithUserAndContent : DocId -> UserId -> String -> NetworkModel
-initWithUserAndContent docId userId content =
-    init (initialServerState docId userId content)
-
-
-initWithUsersAndContent : DocId -> List UserId -> String -> NetworkModel
-initWithUsersAndContent docId userIds content =
-    init (manyUserInitialServerState docId userIds content)
-
-
-updateFromUser : EditEvent -> NetworkModel -> NetworkModel
-updateFromUser event localModel =
-    { localMsgs = localModel.localMsgs ++ [ event ]
-    , serverState = localModel.serverState
-    }
-
-
-localState : (EditEvent -> ServerState -> ServerState) -> NetworkModel -> ServerState
-localState updateFunc localModel =
-    List.foldl updateFunc localModel.serverState localModel.localMsgs
-
-
-getLocalDocument : NetworkModel -> OT.Document
-getLocalDocument localModel =
-    localState applyEvent localModel |> .document
-
-
-updateFromBackend : (EditEvent -> ServerState -> ServerState) -> EditEvent -> NetworkModel -> NetworkModel
-updateFromBackend updateFunc event localModel =
-    { localMsgs = List.Extra.remove event localModel.localMsgs
-    , serverState = updateFunc event localModel.serverState
-    }
